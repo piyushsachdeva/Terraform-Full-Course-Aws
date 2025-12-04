09d6524b47642de30757ba84fa1ea80f8dbcdb91
Baivab Mukhopadhyay <mukhopadhyaybaivab77@gmail.com>
2025-12-04 17:03:57 +0530

    Add additional S3 bucket resource for backup with computed name suffix
    Signed-off-by: Baivab Mukhopadhyay <mukhopadhyaybaivab77@gmail.com>

--- lessons/day05/code/main.tf
+++ lessons/day05/code/main.tf
@@ -4,3 +4,10 @@ resource "aws_s3_bucket" "demo" {
 
   tags = local.common_tags # Local variable (tags)
 }
+
+# Additional S3 bucket (backup)
+resource "aws_s3_bucket" "demo_backup" {
+  bucket = "${local.full_bucket_name}-backup" # based on the computed name with a suffix
+
+  tags = local.common_tags
+}
