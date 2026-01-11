22dbd79d24d06282bd256598b87f47f68d07f9bf
mamataghordi <mamataghordi@email.com>
2026-01-11 12:46:22 +0530

    var

--- lessons/day27/code/variables.tf
+++ lessons/day27/code/variables.tf
@@ -10,41 +10,41 @@ variable "environment" {
   default     = "production"
 }
 
-# variable "vpc_cidr" {
-#   description = "The CIDR block for the VPC"
-#   type        = string
-#   default     = "10.0.0.0/16"
-# }
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
 
-# variable "public_subnet_cidrs" {
-#   description = "List of CIDR blocks for public subnets"
-#   type        = list(string)
-#   default     = ["10.0.1.0/24", "10.0.2.0/24"]
-# }
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
 
-# variable "private_subnet_cidrs" {
-#   description = "List of CIDR blocks for private subnets"
-#   type        = list(string)
-#   default     = ["10.0.11.0/24", "10.0.12.0/24"]
-# }
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
 
-# variable "public_subnet_count" {
-#   description = "Number of public subnets to create"
-#   type        = number
-#   default     = 2
-# }
+variable "public_subnet_count" {
+  description = "Number of public subnets to create"
+  type        = number
+  default     = 2
+}
 
-# variable "private_subnet_count" {
-#   description = "Number of private subnets to create"
-#   type        = number
-#   default     = 2
-# }
+variable "private_subnet_count" {
+  description = "Number of private subnets to create"
+  type        = number
+  default     = 2
+}
 
-# variable "availability_zones" {
-#   description = "List of availability zones"
-#   type        = list(string)
-#   default     = ["ap-south-1a", "ap-south-1b"]
-# }
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["ap-south-1a", "ap-south-1b"]
+}
 
 
 
@@ -72,8 +72,8 @@ variable "min_size" {
   default     = 1
 }
 
-# variable "s3_bucket_name" {
-#   description = "The name of the S3 bucket (prefix)"
-#   type        = string
-#   default     = "terraform-day15-prod-bucket"
-# }
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket (prefix)"
+  type        = string
+  default     = "terraform-day15-prod-bucket"
+}
