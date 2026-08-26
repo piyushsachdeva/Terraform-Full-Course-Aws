95057857d2dee44998a99ed078f7fd5e49e24b4e
Sergeyv <slimeril@users.noreply.github.com>
2026-08-26 15:18:10 +0300

    Remove S3 backend configuration from main.tf
    Removed backend configuration for S3 in Terraform.

--- lessons/day29/terraform/main.tf
+++ lessons/day29/terraform/main.tf
@@ -1,10 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "vitninlab-tf-state-prod-gitops"
-    key    = "eks/terraform.tfstate"
-    region = "us-east-1"
-  }
-
 data "aws_availability_zones" "available" {
   filter {
     name   = "opt-in-status"
