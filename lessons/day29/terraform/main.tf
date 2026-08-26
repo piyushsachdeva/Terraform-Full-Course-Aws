53badb2312ebfab3f3b393a9123bb457940a4f1b
Sergeyv <slimeril@users.noreply.github.com>
2026-08-26 14:40:33 +0300

    Upgrade EKS module version to 21.0

--- lessons/day29/terraform/main.tf
+++ lessons/day29/terraform/main.tf
@@ -72,7 +72,7 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "~> 21.0"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.31"
