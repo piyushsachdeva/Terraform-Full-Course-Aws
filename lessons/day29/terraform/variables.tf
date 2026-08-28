7e63ef36eeb6cdd8ee47c7b1284280269e3bc03a
Sergeyv <slimeril@users.noreply.github.com>
2026-08-28 11:03:47 +0300

    Update default Kubernetes version to 1.33

--- lessons/day29/terraform/variables.tf
+++ lessons/day29/terraform/variables.tf
@@ -19,7 +19,7 @@ variable "vpc_cidr" {
 variable "kubernetes_version" {
   description = "Kubernetes version for the EKS cluster"
   type        = string
-  default     = "1.32"
+  default     = "1.33"
 }
 
 variable "admin_principal_arn" {
