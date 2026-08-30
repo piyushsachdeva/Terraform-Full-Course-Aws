b71428d431061603f8a1cca98dfc2ea8c376499f
Sergey Vitnin <vitnin@protonmail.com>
2026-08-30 13:30:20 +0300

    Add Helm provider

--- lessons/day29/terraform/terraform.tf
+++ lessons/day29/terraform/terraform.tf
@@ -20,6 +20,10 @@ terraform {
       source  = "gavinbunney/kubectl"
       version = ">= 1.14.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.13.0"
+    }
     http = {
       source  = "hashicorp/http"
       version = ">= 3.0"
